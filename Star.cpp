4ba85b138dc504188bee51d5717ee31326b5d9af
SneezyDinosaur <51307666+SneezyDinosaur@users.noreply.github.com>
2023-03-03 03:56:46 +0000

    Add files via upload
    Added the basic math functions for converting the star coordinates from Equatorial Coordinates to Cartesian Coordinates.

--- Star.cpp
+++ Star.cpp
@@ -10,3 +10,19 @@ Star::Star()
 Star::~Star()
 {
 }
+
+
+//Converting from polar to 3D cartesian coordinates where:
+//radius:distance, Horizontal rotation(from x-axis):Right Acension, Vertical rotation(from x-axis):Declination. And:
+FVector EquitorialToCartesian(float distance, float rightAcension, float declination)
+{
+    float x;
+    float y;
+    float z;
+
+    x = distance * FMath::Cos(rightAcension);
+    y = distance * FMath::Sin(rightAcension);
+    z = distance * FMath::Cos(declination);
+
+    return Fvector(x,y,z);
+}
